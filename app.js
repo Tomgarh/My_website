ffd76daa3718b54dce4f24caa8c10debef3d3cf9
Thomas Ogar Besong <112964214+Tomgarh@users.noreply.github.com>
2026-02-14 11:38:00 +0100

    Update Explore Nigeria website with images and responsive design

---
+++ app.js
@@ -0,0 +1,14 @@
+function takeNum(num) {
+    return num * num
+}
+let result = takeNum(2)
+console.log(result)
+
+function greet(name) {
+    return "Hello " + name
+}
+console.log(greet("Tom"))
+
+function takesNum(a,b,c) {
+    return a + b + c
+} 
